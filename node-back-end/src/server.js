40289ad21d77c9b3ed10719e2c7de8d5d43b99bd
David González <dp@phoenixconnection.es>
2023-04-18 10:20:24 +0200

    Creating data in MongoDB

--- node-back-end/src/server.js
+++ node-back-end/src/server.js
@@ -46,12 +46,18 @@ async function start() {
     });
 
     // Add item to user cart
-    app.post('/cart', (req, res) => {
+    app.post('/users/:userId/cart/', async (req, res) => {
+        const userId = req.params.userId;
         const productId = req.body.id;
-        cartItems.push(productId);
-        const populatedCart = populateCartIds(cartItems)
+
+        await db.collection('users').updateOne({ id: userId }, {
+            $addToSet: { cartItems: productId }
+        });
+
+        const user = await db.collection('users').findOne({ id: req.params.userId });
+        const populatedCart = await populateCartIds(user.cartItems);
         res.json(populatedCart);
-    })
+    });
 
     // Remove item from user cart
     app.delete('/cart/:productId', (req, res) => {
