caae4754367072031a3cac630777c186b016077f
David González <dp@phoenixconnection.es>
2023-04-18 09:58:34 +0200

    Adding MongoDB to a Node.js server

--- node-back-end/src/server.js
+++ node-back-end/src/server.js
@@ -1,5 +1,16 @@
 import express from 'express';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 import { cartItems as cartItemsRaw, products as productsRaw } from './temp-data';
+
+const uri = "mongodb+srv://davile:OGcetRbVNpcV6UqF@cluster0.gssuz0t.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
 
 let cartItems = cartItemsRaw;
 let products = productsRaw;
@@ -11,8 +22,11 @@ function populateCartIds(ids) {
     return ids.map(id => products.find(product => product.id === id));
 }
 
-app.get('/hello', (req, res) => {
-    res.send('Hello!');
+app.get('/hello', async (req, res) => {
+    await client.connect();
+    const db = client.db('fullstackvue-db');
+    const products = await db.collection('products').find({}).toArray();
+    res.send(products);
 })
 
 // Get all the products
