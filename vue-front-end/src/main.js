fcc3a0d8e726ef62d336bb3de957a54023723e7f
David González <dp@phoenixconnection.es>
2023-04-19 07:47:38 +0200

    Initialize Firebase

--- vue-front-end/src/main.js
+++ vue-front-end/src/main.js
@@ -2,6 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './main.css'
 import router from './router'
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCzHA47adZeMqQY44ZCgf_oC2c-eDth1Nk",
+  authDomain: "vue-full-stack-ecommerce-site.firebaseapp.com",
+  projectId: "vue-full-stack-ecommerce-site",
+  storageBucket: "vue-full-stack-ecommerce-site.appspot.com",
+  messagingSenderId: "722352460684",
+  appId: "1:722352460684:web:5916c6303db22be69efca9"
+};
+
+initializeApp(firebaseConfig);
 
 createApp(App)
     .use(router)
